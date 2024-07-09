5a127576cf558f2653cba5c9947f0e71a0aef6a3
Sharad Sisodia <sharadsisodia222@gmail.com>
2024-07-09 19:39:39 +0000

    css update

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 // Smooth Scrolling for Anchor Links
 document.querySelectorAll('.header a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
     });
   });
-// document.querySelectorAll('.header a').forEach(anchor => {
-//     anchor.addEventListener('click', function (e) {
-//       e.preventDefault();
-//       const target = document.querySelector(this.getAttribute('href'));
-//       if (target) {
-//         target.scrollIntoView({
-//           behavior: 'smooth'
-//         });
-//       }
-//     });
-//   });
+});
